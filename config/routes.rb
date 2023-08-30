878e2c001041de011d80e8c17c5c2370a2c7320f
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-08-30 16:30:47 +0000

    added routes for square

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get("/", controller: "calc", action: "index")
+  get("/square/new", controller: "calc", action: "index")
   
+  get("/square/results", controller: "calc", action: "square")
 end
