3a29e7c25bf9d899fa7fa5d29797682f5b8ff9b7
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-08-30 16:40:15 +0000

    added actions for square root

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -1,11 +1,22 @@
 class CalcController < ApplicationController
-  def index
+  #square actions
+  def square
     render template: "calc/square"
   end
 
-  def square
+  def square_result
     @number = params.fetch("square").to_i
     @square = @number ** 2
     render template: "calc/square_result"
   end
+
+  #root actions
+  def root
+    render template: "calc/root"
+  end
+
+  def root_result
+    render template: "calc/root_result"
+  end
+
 end
