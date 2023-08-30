d5e58878d6339ba3412813a00570358327381c2f
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-08-30 16:40:06 +0000

    added routes for square root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get("/", controller: "calc", action: "index")
-  get("/square/new", controller: "calc", action: "index")
-  
-  get("/square/results", controller: "calc", action: "square")
+  # Square calc routes
+  get("/", controller: "calc", action: "square")
+  get("/square/new", controller: "calc", action: "square")
+
+  get("/square/results", controller: "calc", action: "square_result")
+
+  # Square root calc routes
+  get("/square_root/new", controller: "calc", action: "root")
+  get("/square_root/results", controller: "calc", action: "root_result")
 end
